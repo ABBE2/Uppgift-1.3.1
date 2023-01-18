c01dceaaa524035d1d92afbcc9672c4a4e7ac492
BaoLanNguyen <bang03001@utb.vaxjo.se>
2023-01-18 10:19:01 +0100

    Add project files.

---
+++ Uppgift 1.3.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace uppgift_1._3
+{
+
+    class program3
+    {
+
+        static void Main(string[] args)
+        {
+            //Triangeln
+            Console.WriteLine("   /\\");
+            Console.WriteLine("  /  \\");
+            Console.WriteLine(" / \"\" \\");
+            Console.WriteLine("/______\\");
+
+        }
+
+    }
+
+}
